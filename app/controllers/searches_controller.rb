9012eb271195b449df22df4b83076b1b29f575ab
EC2 Default User <129925697+Jack-00000@users.noreply.github.com>
2023-05-10 07:25:53 +0000

    update

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,14 @@
 class SearchesController < ApplicationController
-    
-  def search
-    @book = Book.search(params[:keyword])
-  end
-  
+  before_action :authenticate_user!
+
+	def search
+		@model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		if @model == 'user'
+			@records = User.search_for(@content, @method)
+		else
+			@records = Book.search_for(@content, @method)
+		end
+	end
 end
